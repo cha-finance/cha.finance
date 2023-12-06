67babbf8be97863f639c0fdb72525b26a8799b43
Quy Nguyen <bk.nguyenvanquy@gmail.com>
2023-12-06 16:55:40 +0700

    fix: chain

--- src/wagmi.ts
+++ src/wagmi.ts
@@ -1,14 +1,11 @@
 import { configureChains, createConfig } from "wagmi";
-import { bsc, goerli, mainnet } from "wagmi/chains";
-import { CoinbaseWalletConnector } from "wagmi/connectors/coinbaseWallet";
-import { InjectedConnector } from "wagmi/connectors/injected";
+import { mainnet } from "wagmi/chains";
 import { MetaMaskConnector } from "wagmi/connectors/metaMask";
-import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
 
 import { publicProvider } from "wagmi/providers/public";
 
 const { chains, publicClient, webSocketPublicClient } = configureChains(
-  [mainnet, bsc,
+  [mainnet,
     //  ...(import.meta.env?.MODE === "development" ? [goerli] : [])
     ],
   [publicProvider()]
