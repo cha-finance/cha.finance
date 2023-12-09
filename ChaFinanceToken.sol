3d68294d0b12f8b9955333f44e19ff49b80c16aa
TungHoang <hoangtung.utc@gmail.com>
2023-12-09 23:23:29 +0700

    update

--- ChaFinanceToken.sol
+++ ChaFinanceToken.sol
@@ -4,13 +4,23 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
-contract OnChainClash is ERC20 {
+contract ChaFinance is ERC20 {
     using SafeERC20 for IERC20;
     IERC20 private LUA = IERC20(address(0xB1f66997A5760428D3a87D68b90BfE0aE64121cC));
-    uint public cap = 500000000 * 10e18;
+    uint public cap = 500000000 * 1e18;
 
     constructor() ERC20("Cha Finance", "CHA") {
+        if (getChainID() == 1) { // only ETH
+            _mint(msg.sender, 50000000 * 1e18); // send to owner to manually send LUA onwer on Viction
+        }
+    }
 
+    function getChainID() public view returns (uint256) {
+        uint256 id;
+        assembly {
+            id := chainid()
+        }
+        return id;
     }
 
     function mint(uint amount) external {
